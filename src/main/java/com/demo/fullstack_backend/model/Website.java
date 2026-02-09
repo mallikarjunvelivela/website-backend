468bff9198544cac6dff90aab036b7257de1d3bf
mallikarjunvelivela <mallikarjunvelivela@gmail.com>
2026-02-09 11:18:03 +0530

    frontend connection  changes

--- src/main/java/com/demo/fullstack_backend/model/Website.java
+++ src/main/java/com/demo/fullstack_backend/model/Website.java
@@ -1,16 +1,18 @@
 package com.demo.fullstack_backend.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Table(name = "websites")
 public class Website {
 
     @Id
